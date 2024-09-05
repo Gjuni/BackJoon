db8033f30371847768e3a587e5275d0e71972553
Gjuni <112748587+Gjuni@users.noreply.github.com>
2024-09-05 22:19:10 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(input.readLine());
+        int result = (int)Math.pow(2, num);
+        output.write(String.valueOf(result));
+
+        input.close();
+        output.flush();
+        output.close();
+    }
+}
