fc64a8ece3eedde34ea476ae03dde27b2c8f662f
Jun <112748587+Gjuni@users.noreply.github.com>
2025-02-10 15:16:54 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 17112 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,39 +6,39 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String argc[]) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer token = new StringTokenizer(input.readLine());
-        int coin_kind = Integer.parseInt(token.nextToken());
+
+        int money_count = Integer.parseInt(token.nextToken());
         int total_money = Integer.parseInt(token.nextToken());
-        int[] coin_array = new int[coin_kind];
-        int count = 0;
-        int compare_num = total_money;
 
-        for(int i =0; i< coin_kind; i++) {
-            coin_array[i] = Integer.parseInt(input.readLine());
+        int[] money_array = new int[money_count];
+
+        for(int i = money_count-1; i >= 0; i--) {
+            money_array[i] = Integer.parseInt(input.readLine());
         }
 
-        int idx = coin_kind-1;
-        while(compare_num != 0) {
-            if(compare_num - coin_array[idx] < 0 && idx >= 0) {
-                idx--;
-            } else if(compare_num - coin_array[idx] > 0) {
-                compare_num = compare_num - coin_array[idx];
-                count++;
-            } else if(compare_num - coin_array[idx] == 0) {
-                compare_num -= coin_array[idx];
-                count++;
-                break;
+        int answer_count = 0;
+        int index =0;
+
+        while (total_money > 0) { 
+            if(total_money >= money_array[index]) {
+                total_money -= money_array[index];
+                answer_count++;
+            } else {
+                index++;
             }
-        }
+        }   
 
-        output.write(String.valueOf(count));
+        output.write(String.valueOf(answer_count));
+
         input.close();
-        output.flush();
         output.close();
-    }
+    }    
+    
 }
