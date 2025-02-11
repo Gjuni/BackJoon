100833e6ced1afe5f004fdb1f6b296fb8045c70b
Jun <112748587+Gjuni@users.noreply.github.com>
2025-02-11 14:23:57 +0900

    [Silver III] Title: 소수 구하기, Time: 984 ms, Memory: 29564 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,36 +1,51 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 class Main {
+    public static int isPrime(int num) {
+        if(num == 1) {
+            return 0;
+        }
+
+        if(num == 2) {
+            return 1;
+        }
+
+        for(int i = 2; i<= Math.sqrt(num); i++) {
+            if(num%i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    
+    public static void solve(int min_num, int max_num) {
+        for(int i = min_num; i<= max_num; i++) {
+            int answer = isPrime(i);
+            
+            if(answer == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer token =  new StringTokenizer(input.readLine());
-        int first_prime = Integer.parseInt(token.nextToken());
-        int second_prime = Integer.parseInt(token.nextToken());
-        HashSet<Integer> non_prime = new HashSet<Integer>();
+        StringTokenizer token = new StringTokenizer(input.readLine());
 
-        for(int i = 2; i <= second_prime/2; i++) {
-            for(int j = 2; i*j <= second_prime; j++) {
-                non_prime.add(i*j);
-            }
-        }
+        int min_num = Integer.parseInt(token.nextToken());
+        int max_num = Integer.parseInt(token.nextToken());
 
-        for(int f = first_prime; f <= second_prime; f++) {
-            if(f == 1) {
-                ;
-            } else if(!non_prime.contains(f)) {
-                output.write(String.valueOf(f)+"\n");
-            }
-        }
-        output.flush();
-        output.close();
+        solve(min_num, max_num);
+
         input.close();
+        output.close();
     }
 }
