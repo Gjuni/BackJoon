87775191f53c52e6c557a905386827b7bafd0c80
Jun <112748587+Gjuni@users.noreply.github.com>
2025-02-11 14:44:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1520 ms, Memory: 171872 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,33 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String argc[]) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter ouput = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int repeat_num = Integer.parseInt(input.readLine());
-        int num_array[] = new int[repeat_num];
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
 
         for(int i =0; i< repeat_num; i++) {
-            num_array[i] = Integer.parseInt(input.readLine());
+            int num = Integer.parseInt(input.readLine());
+            array.add(num);
         }
-        Arrays.sort(num_array);
 
-        for(int i : num_array) {
-            ouput.write(i + "\n");
-        }        
-        ouput.flush();
-        ouput.close();
+        Collections.sort(array);
+
+        for(int i = 0; i< repeat_num; i++) {
+            output.write(String.valueOf(array.get(i))+"\n");
+        }
+
+        input.close();
+        output.close();
     }
 }
