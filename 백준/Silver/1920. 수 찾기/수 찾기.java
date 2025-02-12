e605220ef095759330de2a06f92d65f173c34edb
Jun <112748587+Gjuni@users.noreply.github.com>
2025-02-12 14:48:31 +0900

    [Silver IV] Title: 수 찾기, Time: 772 ms, Memory: 50680 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,50 +3,59 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 class Main {
-    public static int binsearch(int[] arr, int key) {
-        int low = 0;
-        int high = arr.length-1;
+    public static int _binarysearch(int n, ArrayList<Integer> answer_sheet) {
+        int start = 0;
+        int end = answer_sheet.size()-1;
 
-        while(low <= high) {
-            int mid = (low + high)/2;
+        while(start <= end) {
+            int middle = (start+end)/2;
 
-            if(arr[mid] < key) {
-                low = mid+1;
-            } else if(arr[mid] > key) {
-                high = mid -1;
-            } else {
+            if(n == answer_sheet.get(middle)) {
                 return 1;
+            } else if(n > answer_sheet.get(middle)) {
+                start = middle+1;
+            } else if(n < answer_sheet.get(middle)) {
+                end = middle-1;
             }
         }
         return 0;
     }
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String args[]) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int list_num = Integer.parseInt(input.readLine());
-        String list_string = input.readLine().trim();
-        StringTokenizer token1 = new StringTokenizer(list_string);
-        int[] list_array = new int[list_num];
-        for(int i =0; i< list_num; i++) {
-            list_array[i] = Integer.parseInt(token1.nextToken());
-        }
-        Arrays.sort(list_array);
+        // 해답지
+        int N = Integer.parseInt(input.readLine());
+        ArrayList<Integer> answer_sheet = new ArrayList<Integer>();
+        StringTokenizer token = new StringTokenizer(input.readLine());
 
-        int find_num = Integer.parseInt(input.readLine());
-        StringTokenizer token2 = new StringTokenizer(input.readLine());
+        for(int i =0; i< N; i++) {
+            answer_sheet.add(Integer.parseInt(token.nextToken()));
+        }
+        Collections.sort(answer_sheet);
         
-        for(int i =0; i< find_num; i++) {
-            int find_value = Integer.parseInt(token2.nextToken());
-            output.write(binsearch(list_array, find_value) + "\n");
+
+        // N과 비교해서 정답 출력할 수들
+        int M = Integer.parseInt(input.readLine());
+        int[] cmp_array = new int[M];
+        token = new StringTokenizer(input.readLine());
+
+        for(int i =0; i< M; i++) {
+            cmp_array[i] = Integer.parseInt(token.nextToken());
         }
 
-        output.flush();
-        output.close();
+        for(int i =0; i< M; i++) {
+            int result = _binarysearch(cmp_array[i], answer_sheet);
+            output.write(String.valueOf(result)+"\n");
+        }
+    
         input.close();
+        output.close();
     }
 }
