de99635dea374cfc37cb8f00c0040b03c3c99f8f
Jun <112748587+Gjuni@users.noreply.github.com>
2025-02-21 15:44:32 +0900

    [Silver II] Title: 최대 힙, Time: 300 ms, Memory: 32448 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,3 +1,22 @@
+/**
+ *  문제 이름 : 최대 힙
+ * 
+ *  난 이  도 : 실버 2
+ * 
+ *  아이디어 : 
+ *              x에 0이 주어지면 숫자 poll
+ *              x에 0이 아닌 다른 수를 주면 push 사용용
+ * 
+ * 
+ *  해    설 : 우선 순위 큐 내림차순하려면 reverseOrder를 사용해야함 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,26 +24,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Collections;
 import java.util.PriorityQueue;
+
 class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        PriorityQueue<Integer> max_q = new PriorityQueue<>(Collections.reverseOrder());
+
         int repeat_num = Integer.parseInt(input.readLine());
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
         for(int i =0; i< repeat_num; i++) {
             int num = Integer.parseInt(input.readLine());
-            
+
             if(num == 0) {
-                if(maxHeap.isEmpty()) {
+                if(max_q.isEmpty()) {
                     output.write(String.valueOf(0)+"\n");
                 } else {
-                    output.write(String.valueOf(maxHeap.poll())+"\n");
+                    output.write(String.valueOf(max_q.poll())+"\n");
                 }
             } else {
-                maxHeap.offer(num);
+                max_q.offer(num);
             }
-            
+
         }
 
         input.close();
