16da228843dd5689daeb6a60e971df2108cde8c6
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-03 03:11:46 +0900

    [Silver V] Title: 수들의 합, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,39 +1,48 @@
+/**
+ *  문제 이름 : 수들의 합
+ * 
+ *  난 이  도 : 실버 5
+ * 
+ *  아이디어 : 서로 다른 N개의 자연수의 합을 S라고 함
+ *              S를 알 때, 자연수 N의 최댓값은?
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-        long num = Long.valueOf(input.readLine());
-        long total = 0;
-        long i =0;
-        int count = 0;
 
-        while(true) {
-            if(num == 1 || num == 2) {
-                count++;
-                break;
-            } else if(total == num) {
-                break;
-            }
-            
-            if(total < num) {
-                i++;
-                total += i;
-                count++;
-            } else if(total > num) {
-                count--;
-                break;
-            }
+        long num = Long.parseLong(input.readLine());
+
+        int idx = 0;
+        long added_num = 0;
+
+        while(added_num <= num) {
+            ++idx;
+            added_num += idx;
         }
-        output.write(String.valueOf(count));
+
+        if(added_num == num) {
+            output.write(String.valueOf(idx));
+        } else {
+            output.write(String.valueOf(idx-1));
+        }
+
 
         input.close();
-        output.flush();
         output.close();
     }
 }
