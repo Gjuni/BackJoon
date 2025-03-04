ee9ee9d7cc71f02bcc6a2ae40704e9fa7ed0ae21
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-04 12:38:39 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 204 ms, Memory: 19124 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,3 +1,18 @@
+/**
+ *  문제 이름 : 균형잡힌 세상
+ * 
+ *  난 이  도 : 실버 4
+ * 
+ *  아이디어 : ()와 []가 짝을 이루어야한다.
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,47 +20,63 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static boolean check(Stack<Character> stack, Character cmp_char) {
+        if(stack.isEmpty()) {
+            return false;
+        } else {
+            Character top_stack = stack.peek();
+
+            if((top_stack == '(' && cmp_char == ')') || (top_stack == '[' && cmp_char == ']')) {
+                stack.pop();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String argc[]) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        while(true){
-            Stack<Character> stack = new Stack<>();
-            String nextLine = input.readLine();
-            
-            if(nextLine.equals(".")) {
-                break;
-            }
-            boolean check = true;
+        boolean result = true;
 
-            for(int i =0;i < nextLine.length(); i++)
-            {
-                char world = nextLine.charAt(i);
-                if(world== '(' || world == '[') {
-                    stack.push(world);
-                } else if(world == ')') {
-                    if(stack.isEmpty() || stack.pop() != '(') {
-                        check = false;
-                        break;
-                    }
-                } else if(world == ']') {
-                    if(stack.isEmpty() || stack.pop() != '[') {
-                        check = false;
-                        break;
-                    }
+        String string = input.readLine();
+
+        while(!string.equals(".")) {
+            result = true;
+
+            Stack<Character> parentheses = new Stack<>();
+
+            for(int i =0; i < string.length(); i++) {
+                char cmp_char = string.charAt(i);
+
+                if(cmp_char == '(' || cmp_char =='[') {
+                    parentheses.add(cmp_char);
+                } else if(cmp_char == ')' || cmp_char == ']') {
+                    result = check(parentheses, cmp_char);
                 }
-            } 
-            if(check && stack.isEmpty()) {
+
+                if(!result) {
+                    break;
+                }
+
+            }
+
+
+            if(result && parentheses.isEmpty()) {
                 output.write("yes\n");
             } else {
                 output.write("no\n");
             }
+
+            string = input.readLine();
         }
-    
-        output.flush();
-        output.close();
+
+
+
         input.close();
+        output.close();
     }
 }
-
