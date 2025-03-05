5475954a293bb45cf2be76c35e2ee0b68d393f46
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-05 13:09:57 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 36872 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,39 +1,69 @@
+/**
+ *  문제 이름 : 카드 2
+ * 
+ *  난 이  도 : 실버 4
+ * 
+ *  아이디어 : 1부터 N까지의 번호가 붙어있는 카드가 존재한다.
+ *              이때 1이 제일 위에 있고 N이 제일 밑에 존재한다.
+ * 
+ *             1. 제일 위에 있는 카드를 바닥에 버린다.
+ *             2. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+ * 
+ *              제일 마지막에 남는 카드를 구하는 프로그램을 작성하시오.
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
-class Main
-{
+public class Main {
+    public static int solve(int number_card, Queue<Integer> q) {
+        int answer = 0;
+        int index = 1;
+
+        while(q.size() > 1) {
+            int current_num = q.poll();
+
+            if(index%2== 1) {
+                
+            } else if(index%2 ==0) {
+                q.add(current_num);
+            }
+
+            index++;
+        }
+
+        answer = q.poll();
+
+        return answer;
+    }   
+
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int card_num = Integer.parseInt(input.readLine());
-        Queue<Integer> card = new LinkedList<>();
+        int number_card = Integer.parseInt(input.readLine());
 
-        card.clear();
-        for(int i =0; i< card_num; i++) {
-            card.add(i+1);
-        }
-        int idx =0;
+        Queue<Integer> q = new ArrayDeque<>();
 
-        while(card.size() > 1) {
-            idx++;
-            if(idx%2 != 0) {
-                card.remove();
-            } else if(idx%2 == 0) {
-                card.add(card.remove());
-            }
+        for(int i =1; i<= number_card; i++) {
+            q.add(i);
         }
 
-        output.write(String.valueOf(card.peek()));
+        output.write(String.valueOf(solve(number_card, q)));
 
-        output.flush();
-        output.close();
         input.close();
+        output.close();
     }
 }
