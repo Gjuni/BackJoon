c5c0ef31bfe2f35c67d538009b6e40db64e91f5c
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-05 12:42:55 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -5,44 +5,49 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static String solved(String parthenes) {
 
-        int repeat_num = Integer.parseInt(input.readLine());
-        
-        for(int i =0; i < repeat_num; i++) {
-            String parenthes = input.readLine();
-            Stack<Character> stack = new Stack<>();
-            boolean canornot = true;
+        Stack<Character> stack = new Stack<>();
 
-            for(int j =0; j < parenthes.length(); j++) {
-                char ch = parenthes.charAt(j); // 한글자씩 읽어들임
+        for(int i =0; i< parthenes.length(); i++) {
+            char sepaerate = parthenes.charAt(i);
+
+            if(sepaerate == '(') {
+                stack.add(sepaerate);
+            } else if (sepaerate == ')') {
                 
-                if(ch == '(') {
-                    stack.push(ch);
-                } else if(ch == ')') {
-                    if(stack.empty()) {
-                        canornot = false;
-                        break;
-                    } else {
+                if(stack.isEmpty()) {
+                    return "NO";
+                } else {
+                    char temp_char = stack.peek();
+
+                    if(temp_char == '(') {
                         stack.pop();
                     }
                 }
             }
-            if(!stack.empty()) {
-                canornot = false;
-            }
-
-            if(canornot) {
-                output.write("YES\n");
-            } else {
-                output.write("NO\n");
-            }
         }
-        output.flush();
-        output.close();
+
+        if(stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+    public static void main(String argc[]) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(input.readLine());
+
+        for(int i =0; i< num; i++) {
+            String parthenes = input.readLine();
+
+            output.write(solved(parthenes)+"\n");
+        }
+
         input.close();
+        output.close();
     }
 }
