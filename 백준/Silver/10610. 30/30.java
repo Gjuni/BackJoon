aaab2bc49b65027646209f4d7fe22bb526936e33
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-16 02:19:10 +0900

    [Silver IV] Title: 30, Time: 280 ms, Memory: 24204 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,41 +1,62 @@
+/**
+ *  문제 이름 : 30
+ * 
+ *  난 이  도 : 실버 4
+ * 
+ *  아이디어 : 수가 주어지면 30으로 만들 수 있는 최대의 수로 만들어라.
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        String num1 = input.readLine();
-        boolean check = false;
-        int total = 0;
-        Integer temp[] = new Integer[num1.length()];
-        for(int i =0; i< num1.length(); i++) {
-            temp[i] = num1.charAt(i)-'0';
-            total += temp[i];
 
-            if(temp[i] == 0) {
-                check = true;
+        
+        String string = input.readLine();
+
+        boolean result = false;
+
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            int current_num = string.charAt(i)-'0';
+            answer += current_num;
+            list.add(current_num);
+
+            if(current_num == 0) {
+                result = true;
             }
         }
-        
-        if(total%3 == 0 && check) {
 
-            Arrays.sort(temp, Collections.reverseOrder());
-            for(int i =0; i< num1.length(); i++) {
-                output.write(String.valueOf(temp[i]));
+        Collections.sort(list);
+
+        if(answer%3 == 0 && result) {
+            for(int i = list.size()-1; i >= 0; i--) {
+                output.write(String.valueOf(list.get(i)));
             }
         } else {
             output.write(String.valueOf(-1));
         }
 
+        
+
         input.close();
-        output.flush();
         output.close();
     }
 }
