a7d08b21a9aa2b6a1be041595beeadc16fc6dd99
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-17 15:45:13 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,3 +1,18 @@
+/**
+ *  문제 이름 : 유기농 배추
+ * 
+ *  난 이  도 : 실버 2
+ * 
+ *  아이디어 : 
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,65 +20,67 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    static int answer = 0;
+    static int width;
     static int height;
-    static int weight;
-    public static void add(int[][] graph ,int x, int y) {
-        graph[x][y] = 1;
-    }
 
-    public static void result(int[][] graph, int h, int w){
-        if((h >= 0 && height > h) && (w >= 0 && weight > w) && graph[h][w]==1) {
-            graph[h][w] = 0;
-            result(graph, h+1, w);
-            result(graph, h-1, w);
-            result(graph, h, w+1);
-            result(graph, h, w-1);
+    public static void solve(int[][] arr, int w, int h) {
+        if((height > h && h >= 0) && (width > w && w >= 0)) {
+            if(arr[h][w] == 1) {
+                arr[h][w] = 0;
+                solve(arr, w+1, h);
+                solve(arr, w-1, h);
+                solve(arr, w, h+1);
+                solve(arr, w, h-1);
+            }
         }
     }
 
-    public static void main(String[] args) throws IOException{
+
+    public static void add_graph(int [][]arr, int x, int y) {
+        arr[y][x] = 1;
+    }
+
+    public static void main(String[] args) throws IOException { 
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int repeat_num = Integer.parseInt(input.readLine());
 
-        for(int i =0; i< repeat_num; i++) {
-            StringTokenizer token1 = new StringTokenizer(input.readLine());
+        for (int i = 0; i < repeat_num; i++) {
+            answer = 0;
+            StringTokenizer token = new StringTokenizer(input.readLine());
 
-            height = Integer.parseInt(token1.nextToken());
-            weight = Integer.parseInt(token1.nextToken());
-            int node = Integer.parseInt(token1.nextToken());
+            width = Integer.parseInt(token.nextToken());
+            height = Integer.parseInt(token.nextToken());
+            int total_bechu = Integer.parseInt(token.nextToken());
 
-            int graph[][] = new int[height][weight];
+            int arr[][] = new int[height][width];
 
-            for(int j =0; j< node; j++) {
-                StringTokenizer xy = new StringTokenizer(input.readLine());
+            for (int j = 0; j < total_bechu; j++) {
+                token = new StringTokenizer(input.readLine());
 
-                int x = Integer.parseInt(xy.nextToken());
-                int y = Integer.parseInt(xy.nextToken());
+                int x = Integer.parseInt(token.nextToken());
+                int y = Integer.parseInt(token.nextToken());
+                
+                add_graph(arr, x, y);
+            }
 
-                add(graph, x, y);
-            }   
-            int count = 0;
 
-            for(int h =0; h< height; h++) {
-                for(int w = 0; w < weight; w++) {
-                    if(graph[h][w] == 1) {
-                        result(graph, h, w);
-                        count++;
+            for (int h = 0; h < height; h++) {
+                for (int w = 0; w < width; w++) {
+                    if(arr[h][w] == 1) {
+                        solve(arr, w, h);
+                        answer++;
                     }
                 }
             }
-            output.write(String.valueOf(count));
 
-            if(i < repeat_num-1) {
-                output.write("\n");
-            }
+            output.write(String.valueOf(answer)+"\n");
         }
 
         input.close();
-        output.flush();
         output.close();
     }
 }
