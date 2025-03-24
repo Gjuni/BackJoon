ac931f5ddf9e715259048b5a4f448ae677011d07
Jun <112748587+Gjuni@users.noreply.github.com>
2025-03-24 11:52:20 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 524 ms, Memory: 57864 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,40 +1,57 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+/**
+ *  문제 이름 : 나는야 포켓몬 마스터 이다솜
+ * 
+ *  난 이  도 : 실버 4
+ * 
+ *  아이디어 : 번호가 1번 부터인 포켓몬 이름이 주어진다.    
+ *             도감이니 이름이나 도감 번호를 알려주면 그 포켓몬을 알려준다.
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+import java.io.*;
+import java.util.*;
 
-        StringTokenizer num = new StringTokenizer(input.readLine());
-        HashMap<String, String> pictorial = new HashMap<>();
-        HashMap<String, String> pictorial_num = new HashMap<>();
-        int number_of_pokimon = Integer.parseInt(num.nextToken());
-        int find_pokimon = Integer.parseInt(num.nextToken());
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i =1; i<= number_of_pokimon; i++) {
-            String name_pokimon = input.readLine();
-            String num2 = Integer.toString(i);
-            pictorial.put(name_pokimon, num2);
-            pictorial_num.put(num2, name_pokimon);
+    public static void main(String argc[]) throws IOException{
+        
+        StringTokenizer token = new StringTokenizer(input.readLine());
+
+        int repeat_num = Integer.parseInt(token.nextToken());
+        int answer_num = Integer.parseInt(token.nextToken());
+
+        HashMap<String,Integer> name_hash = new HashMap<>();
+        HashMap<String, String> number_hash = new HashMap<>();
+
+
+        for(int i = 1; i<= repeat_num; i++) {
+            String name = input.readLine();
+
+            name_hash.put(name, i);
+            number_hash.put(String.valueOf(i), name);
         }
 
-        for(int i =1; i<= find_pokimon; i++) {
-            String find = input.readLine();
+        for (int i = 0; i < answer_num; i++) {
+            String answer = input.readLine();
 
-            if(pictorial.containsKey(find)) {
-                output.write(pictorial.get(find)+"\n");
-            } else if(pictorial_num.containsKey(find)){
-                output.write(pictorial_num.get(find)+"\n");
+            if(name_hash.containsKey(answer)) {
+                output.write(name_hash.get(answer)+"\n");
+                continue;
+            } else {
+                output.write(number_hash.get(answer)+"\n");
+                continue;
             }
         }
+
         input.close();
-        output.flush();
         output.close();
     }
 }
