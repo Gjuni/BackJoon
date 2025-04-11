1e2a266414d979437b5a2dcab772caab18589172
Jun <112748587+Gjuni@users.noreply.github.com>
2025-04-11 13:39:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,39 +1,63 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+/**
+ *  문제 이름 : 1,2,3 더하기
+ * 
+ *  난 이  도 : 실버 3
+ * 
+ *  아이디어 : 정수를 1,2,3의 합으로 나타내는 가시의 수를 계산하라
+ *            수를 1개 이상 사용해야한다. (1개도 가능)
+ * 
+ *  해    설 : 
+ *                  n = 1 count = 1
+ *                  n = 2 count = 2 
+ *                  n = 3 count = 4 
+ *                  n = 4 count = 7 
+ *                  n = 5 count = 13 
+ *                  n = 6 count = 24               
+ *                  n = 7 count = 44 
+ * 
+ *                  이렇게 보니... 규칙이 보인다. (시발 해설을 보고 20분 동안 지랄해서 얻었다.)
+ *                  결국 DP[i] = DP[i-1] + DP[i-2] + DP[i-3] 이 된다.
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int repeat_num = Integer.parseInt(input.readLine());
+import java.io.*;
 
-        int[] array = new int[repeat_num];
-        int max = 0;
-        for(int i =0; i< repeat_num; i++) {
-            array[i] = Integer.parseInt(input.readLine());
-            if(max < array[i]) {
-                max = array[i];
+public class Main {
+    public static int[] DP = new int[12];
+
+    public static int solve(int num) {
+        DP[1] = 1;
+        DP[2] = 2;
+        DP[3] = 4;
+
+        if(num > 3) {
+            for(int i = 4; i<= num; i++) {
+                DP[i] = DP[i-1] + DP[i-2] + DP[i-3];
             }
         }
-        int[] save_file = new int[max+1];
-        save_file[0] = 1;
-        save_file[1] = 2;
-        save_file[2] = 4;
 
-        for(int i =3; i <= max; i++) {
-            save_file[i] = save_file[i-1] + save_file[i-2] + save_file[i-3];
+        return DP[num];
+    }
+
+
+    public static void main(String argc[]) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(input.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            int num = Integer.parseInt(input.readLine());
+
+            int answer = solve(num);
+
+            output.write(String.valueOf(answer)+'\n');
         }
-        
-        for(int i =0; i< repeat_num; i++) {
-            output.write(String.valueOf(save_file[array[i]-1])+"\n");
-        }
-         
+
         input.close();
-        output.flush();
         output.close();
     }
 }
