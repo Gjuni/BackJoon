d894dba1b65d1c718b078371667f2899be6453df
Jun <112748587+Gjuni@users.noreply.github.com>
2025-04-16 12:53:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 688 ms, Memory: 59408 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,42 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+/**
+ *  문제 이름 : 좌표 정렬하기
+ * 
+ *  난 이  도 : 실버 5
+ * 
+ *  아이디어 : x좌표 증가하는 순서대로 단 x좌표가 동일하면 y좌표로 정렬
+ * 
+ *  해    설 : 
+ *              class 선언 후 Comparetor를 사용.
+ *              compare를 사용하여 sort시킴             
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
+
+ import java.io.*;
+import java.util.ArrayList; 
 import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        //2차원 x좌표 기준으로 정렬
-        // x좌표가 동일하다면 y좌표 기준으로 정렬
+class coordinate {
+    int x;
+    int y;
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int repeat_num = Integer.parseInt(input.readLine());
-        int[][] num_array = new int[repeat_num][2];
-
-        for(int i =0; i< repeat_num; i++) {
-            StringTokenizer token1 = new StringTokenizer(input.readLine());
-            num_array[i][0] = Integer.parseInt(token1.nextToken());
-            num_array[i][1] = Integer.parseInt(token1.nextToken());
-        }
-
-        Arrays.sort(num_array, (a,b) -> {
-            if(a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]);
-            } else {
-                return Integer.compare(a[0], b[0]);
-            }
-        }) ;
-
-        for(int i =0; i< repeat_num; i++) {
-            output.write(String.valueOf(num_array[i][0]+" "+num_array[i][1]+"\n"));
-        }
-        output.flush();
-        output.close();
-        input.close();
+    public coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
+
+ public class Main {
+     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+     static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+     public static void main(String[] args) throws IOException{
+         int repeat_num = Integer.parseInt(input.readLine());
+        List<coordinate> list = new ArrayList<>();
+
+        for (int i = 0; i < repeat_num; i++) {
+            StringTokenizer token = new StringTokenizer(input.readLine());
+
+            int x = Integer.parseInt(token.nextToken());
+            int y = Integer.parseInt(token.nextToken());
+
+            coordinate c = new coordinate(x, y);
+            list.add(c);
+        }
+        
+        list.sort(new Comparator<coordinate>() {
+            @Override
+            public int compare(coordinate o1, coordinate o2) {
+                if(o1.x == o2.x) {
+                    return o1.y - o2.y;
+                } else {
+                    return o1.x - o2.x;
+                }
+            }
+        });
+
+        for(coordinate i : list) {
+            output.write(String.valueOf(i.x) + " "+String.valueOf(i.y)+"\n");
+        }
+
+        input.close();
+        output.close();
+     }
+ }
