2b12ae9615b4fd1e9054c3c36f2097c1a2d03ff2
Jun <112748587+Gjuni@users.noreply.github.com>
2025-04-28 20:09:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1040 ms, Memory: 169748 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,37 +1,62 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+/**
+ *  문제 이름 : 
+ * 
+ *  난 이  도 : 
+ * 
+ *  아이디어 : 
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
+
+import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-        HashMap<Integer, Integer> h1 = new HashMap<Integer, Integer>();
+ public class Main {
+     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+     static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int repeat_num = Integer.parseInt(input.readLine());
-        StringTokenizer token1 = new StringTokenizer(input.readLine());
-        for(int i =0; i< repeat_num; i++) {
-            int now_token = Integer.parseInt(token1.nextToken());
-            int count = h1.getOrDefault(now_token, 0); // 해당 값이 존재하면 0으로 초기 설졍시킴
-            h1.put(now_token, count+1); 
+     public static void main(String[] args) throws IOException{
+        int N = Integer.parseInt(input.readLine());
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer token = new StringTokenizer(input.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(token.nextToken());
+
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
 
-        int key_num = Integer.parseInt(input.readLine());
-        StringTokenizer token2 = new StringTokenizer(input.readLine());
+        int M = Integer.parseInt(input.readLine());
 
-        for(int i=0; i< key_num; i++) {
-            int get_key = Integer.parseInt(token2.nextToken());
-            int count = h1.getOrDefault(get_key,0);
-            h1.put(get_key, count);
-            output.write(String.valueOf(h1.get(get_key))+" ");
+        int[] count_arr = new int[M];
+
+        token = new StringTokenizer(input.readLine());
+
+        for (int i = 0; i < M; i++) {
+            count_arr[i] = Integer.parseInt(token.nextToken());
         }
 
-        output.flush();
-        output.close();
-        input.close();
-    }
-}
+        for (int i = 0; i < M; i++) {
+            int value = map.getOrDefault(count_arr[i], 0);
+           
+            output.write(String.valueOf(value));
+
+            if(i < M-1) {
+                output.write(' ');
+            }
+        }
+
+    
+         input.close();
+         output.close();
+     }
+ }
