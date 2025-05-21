81c49ea27b4a44d6021f7ac9e05e457268f1ecdf
Jun <112748587+Gjuni@users.noreply.github.com>
2025-05-21 12:06:11 +0900

    [Silver IV] Title: 토너먼트, Time: 132 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,31 +1,49 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+/**
+ *  문제 이름 : 토너먼트
+ * 
+ *  난 이  도 : 실버 4
+ * 
+ *  아이디어 : 
+ *              N명이 참가하는 토너먼트
+ *              김지민과 임영웅이 토너먼트를 하는데
+ *              몇 번째 라운드에서 만나는지 궁금하다. 구하라.
+ * 
+ *  해    설 : 
+ *              김지민과 임영웅의 번호가 같아질 때까지 라운드 수를 증가시킨다.
+ *              본인 번호 - (본인 번호/2) 를하여 순위를 줄이기 시작
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
 
-class Main {
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer token = new StringTokenizer(input.readLine());
 
-        String string = input.readLine();
-        int tour[] = new int[3];
-        for(int i =0; i< 3; i++) {
-            String[] temp_array = string.split(" ");
-            tour[i] = Integer.parseInt(temp_array[i]);
-        }
-        
-        int count =0;
+        int N = Integer.parseInt(token.nextToken());
+        int K = Integer.parseInt(token.nextToken());
+        int L = Integer.parseInt(token.nextToken());
 
-        while(tour[1] != tour[2]) {
-            tour[1] = (tour[1]+1)/2;
-            tour[2] = (tour[2]+1)/2;
-            count++;
+        int answer = 0;
+
+        while(K != L) {
+            answer++;
+
+            K -= K/2;
+            L -= L/2;
         }
-        output.write(String.valueOf(count));
+
+        output.write(String.valueOf(answer));
+
         input.close();
-        output.flush();
         output.close();
     }
 }
