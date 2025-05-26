3b33992da812f080ac4de6477c6c67b1fc42d7a3
Jun <112748587+Gjuni@users.noreply.github.com>
2025-05-26 14:17:07 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,44 +1,105 @@
+/**
+ *  문제 이름 : 그룹 단어 체커
+ * 
+ *  난 이  도 : 실버 5
+ * 
+ *  아이디어 : 각 문자가 연속해서 나타나는 경우만 그룹 단어라고 지치한다.
+ *              cczbcd는 모두 연속해서 나타난 문자열이지만
+ *              cbcccbc는 연속해서 나타나지 않았기 때문에 그룹 단어가 아니다.
+ * 
+ *  해    설 : 
+ *             
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int repeat_num = Integer.parseInt(input.readLine());
-        String[] array = new String[repeat_num];
-        int result = 0;
-        
-        for(int i =0; i< repeat_num; i++) {
-            array[i] = input.readLine();
-            int count = 0;
-            ArrayList<Character> list = new ArrayList<>();
-            
-            for(int j = 0; j< array[i].length(); j++) {
-                if(j < array[i].length()-1 && array[i].charAt(j) != array[i].charAt(j+1)) {
-                    if(list.contains(Character.valueOf(array[i].charAt(j)))) { // 이미 존재한다면
-                        count++;
-                    } else {
-                        list.add(array[i].charAt(j));
-                    }
+class Count {
+    char alphabet;
+    int count;
+
+    Count(char alphabet, int count) {
+        this.alphabet = alphabet;
+        this.count = count;
+    }
+}
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static Count alpha[] = new Count[26];
+    static int answer = 0;
+
+    public static void solve(String word) {
+        if (word.length() == 1) {
+            answer++;
+            return;
+        }
+
+
+        for (int i = 0; i < word.length()-1; i++) {
+            int curr_alpha_index = word.charAt(i) - 'a';
+            int next_alpga_index = word.charAt(i+1)- 'a';
+
+
+            if(alpha[curr_alpha_index].alphabet == alpha[next_alpga_index].alphabet) {
+                alpha[curr_alpha_index].count++;
+            } else if (alpha[curr_alpha_index].alphabet != alpha[next_alpga_index].alphabet) {
+                
+                if(alpha[next_alpga_index].count == 0) {
+                    alpha[curr_alpha_index].count++;
+                } else if (alpha[next_alpga_index].count > 0) {
+                    return;
                 }
-                if(j == array[i].length()-1 && list.contains(Character.valueOf(array[i].charAt(j)))) {
-                    count++;
-                } 
-            }
-            if(count == 0) {
-                result++;
             }
         }
 
-        output.write(String.valueOf(result));
+        int curr_alpha_index = word.charAt(word.length()-2) - 'a';
+        int next_alpga_index = word.charAt(word.length()-1) - 'a';
+
+
+            if(alpha[curr_alpha_index].alphabet == alpha[next_alpga_index].alphabet) {
+                alpha[next_alpga_index].count++;
+            } else if (alpha[curr_alpha_index].alphabet != alpha[next_alpga_index].alphabet) {
+                
+                if(alpha[next_alpga_index].count == 0) {
+                    alpha[next_alpga_index].count++;
+                } else if (alpha[next_alpga_index].count > 0) {
+                    return;
+                }
+            }
+
+        answer++;
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int tc = Integer.parseInt(input.readLine());
+
+        for (int g = 0; g < tc; g++) {
+
+            for (int i = 0; i < 26; i++) {            
+                alpha[i] = new Count((char)('a'+i), 0);
+            }
+
+            String word = input.readLine();
+
+            solve(word);
+        }
+
+        output.write(String.valueOf(answer));
+
         input.close();
-        output.flush();
         output.close();
     }
 }
