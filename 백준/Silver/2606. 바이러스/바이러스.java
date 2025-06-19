e86ab15fc0572963a382bb811575d0ee2596655b
Jun <112748587+Gjuni@users.noreply.github.com>
2025-06-19 16:20:06 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,50 +1,85 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+/**
+ *  문제 이름 : 바이러스
+ * 
+ *  난 이  도 : 실버 3
+ * 
+ *  아이디어 : 
+ *              1번 컴퓨터가 바이러스에 걸렸다.
+ *              인접한 모든 컴퓨터가 바이러스에 전염된다.
+ *              
+ *              1번 컴퓨터를 제외한 바이러스에 옮겨진 컴퓨터의 갯수를 구하시오.
+ * 
+ *  해    설 : 
+ * 
+ *  시간 복잡도 : 
+ * 
+ *  공간 복잡도 : 
+ *    
+ */
 
 
-class Main {
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int graph[][];
     static boolean visit[];
-    static int result = 0;
-    public static void putEdge(int[][] graph, int x, int y) {
-        graph[x][y] =1;
-        graph[y][x] =1;
-    }
+    static int answer = 0;
 
-    public static int print_edge(int[][] graph,int start, int vertax) {
-        visit[start] = true;
 
-        for(int i = 1; i <= vertax; i++) {
-            if(graph[start][i] == 1 && visit[i] == false) {
-                result++;
-                print_edge(graph, i, vertax);
+    public static int BFS(int one) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        visit[one] = true;
+
+        queue.add(one);
+
+        while(!(queue.isEmpty())) {
+            int curr_node = queue.poll(); // queue 맨 앞 요소 pop
+
+            // 현재 노드의 길이 만큼 배열 탐색
+            for(int i = 1; i < graph[curr_node].length; i++) {
+                int next = graph[curr_node][i]; 
+
+                // 아직 방문하지 않은 node라면?
+                if(next == 1 && visit[i] == false) {
+                    visit[i] = true;
+
+                    queue.add(i);
+                    answer++;
+                }
             }
         }
-        return result;
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int vertax = Integer.parseInt(input.readLine());
-        int node = Integer.parseInt(input.readLine());
-        
-        int[][] graph = new int[vertax+1][vertax+1];
-        visit = new boolean[vertax+1];
 
-        for(int i =0; i< node; i++) {
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int node = Integer.parseInt(input.readLine());
+
+        // 초기화
+        graph = new int[node+1][node+1]; 
+        visit = new boolean[node+1];
+
+        int repeat_num = Integer.parseInt(input.readLine());
+
+        for (int i = 0; i < repeat_num; i++) {
             StringTokenizer token = new StringTokenizer(input.readLine());
 
+            // 그래프에 삽입
             int x = Integer.parseInt(token.nextToken());
             int y = Integer.parseInt(token.nextToken());
-            putEdge(graph, x, y);
+            
+            // 1이라면 인접한 노드임.
+            graph[x][y] = 1;
+            graph[y][x] = 1;
         }
-        output.write(String.valueOf(print_edge(graph,1,vertax)));
+
+        output.write(String.valueOf(BFS(1)));
+
         input.close();
-        output.flush();
         output.close();
     }
 }
